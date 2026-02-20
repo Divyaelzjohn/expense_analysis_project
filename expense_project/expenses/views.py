d591d735be44540f8f26a46cbdf718cdbe117cab
divya <divelzjon@gmail.com>
2026-02-20 21:12:35 +0530

    home page

--- expense_project/expenses/views.py
+++ expense_project/expenses/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Expense
+from django.db.models import Sum
 
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        date = request.POST.get("date")
+        category = request.POST.get("category")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        Expense.objects.create(
+            date=date,
+            category=category,
+            amount=amount,
+            description=description
+        )
+        return redirect("home")
+
+    expenses = Expense.objects.all()
+    total = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
+
+    return render(request, "expenses/home.html", {
+        "expenses": expenses,
+        "total": total
+    })
